099ec833c0356ae957ac6eebbefd7414725fada8
erickgms01 <erickderi98@gmail.com>
2024-02-08 17:01:08 -0300

    adding project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import {FiSearch} from 'react-icons/fi'
+import './styles/style.css'
+import apiCep from './services/api';
+
 
 function App() {
+  const [input, setInput] = useState('');
+  
+  const [cep, setCep] = useState({});
+  async function clickButton() {
+    if  (input === '') {
+      alert('fill in the field!!')
+      return;
+    }
+
+    try{
+      const response = await apiCep.get(`${input}/json`);
+      setCep(response.data)
+      setInput('') 
+    }catch{
+      alert("Postal Code not found!")
+      setInput('')
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="container">
+      <div className="title">Searcher  Postal Code</div>
+
+      <div className="container-input">
+        <input
+        type="text"
+        placeholder="Enter your postal code..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        />
+        <button className='buttonSearch' onClick={clickButton}>
+          <FiSearch size={25} color="#fffff"/>
+        </button>
+      </div>
+      
+      {Object.keys(cep).length > 0 && (
+        <main className='main'>
+          <h2>Postal Code: {cep.cep}</h2>
+          <span> {cep.logradouro}</span>  
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf} </span>
+        </main>
+      )}
+
     </div>
   );
 }
+
+
 
 export default App;
